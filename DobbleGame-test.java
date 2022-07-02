b8dc9eb398ab327562594bad468088d8d1d83f6a
Areyesenc <55030875+Areyesenc@users.noreply.github.com>
2022-07-02 01:38:26 -0400

    Update DobbleGame-test.java

--- DobbleGame-test.java
+++ DobbleGame-test.java
@@ -44,4 +44,40 @@ public class dobblegame {
         
         
 	}
+     // register
+	JButton btnAddClient = new JButton("register");
+	btnregister.addActionListener(new ActionListener() {
+	public void actionPerformed(ActionEvent e) {
+		try {
+			dobblegame.register(textFieldName.getText());
+			javax.swing.JOptionPane.showMessageDialog(null, "jugador agregado con éxito");
+		} catch (dobblegameException e2) {
+			// TODO: handle exception
+			javax.swing.JOptionPane.showMessageDialog(null, "Error "+e2.getCode()+": "+e2.getMessage() );
+			}
+		}
+	});
+	frame.getContentPane().add(btnAddClient, BorderLayout.WEST);
+	
+//menu! 
+	//colocar el frame con lo de arriba ... 
+		
+  int mainMenu() {
+        try {
+            System.out.println();
+            System.out.println();
+            System.out.println("Bienvenido al juego de Dobble!");
+            System.out.println();
+            System.out.println("1. Crear un juego");
+            System.out.println("2. Registrar jugador");
+            System.out.println("3. Jugar!");
+            System.out.println("4. Visualizar estado completo del juego");
+            System.out.println("5. Salir");
+            System.out.print("\t: ");
+            return Integer.parseInt(input.nextLine());
+        }catch (Exception ignored) {
+            System.out.println("debes colocar una de las 5 opciones!!");
+        }
+        return 0;
+    }
 }
